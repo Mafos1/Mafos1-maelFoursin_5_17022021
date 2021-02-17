89e3e3ee6ab142eac629fbfd7667fe74c9f26f04
Mafos1 <74059690+Mafos1@users.noreply.github.com>
2021-02-17 12:12:28 +0100

    1702

---
+++ script.js
@@ -0,0 +1,13 @@
+$().ready(function (){
+    $(".navbar").hide();
+    "use strict"
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 10) { 
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+});
