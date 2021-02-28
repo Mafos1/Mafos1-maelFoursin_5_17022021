48a6e9cef3a213f1500f3e8695cd5efa849e6a69
Mafos1 <74059690+Mafos1@users.noreply.github.com>
2021-02-28 21:17:30 +0100

    2802-3

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ function funRequest() { // Execution fonction de requete
 
     const fetchPick = fetch("https://oc-p5-api.herokuapp.com/api/cameras"); // Fetch de l'api
     const inputJs = document.getElementById("productContainers"); // Lien ou placer les élément html du javascript
-              
+
     fetchPick
-    .then(res =>  res.json()) // Execution de la réponse de l'api récupéré avec fetch , et traduction en JSON
+        .then(res => res.json()) // Execution de la réponse de l'api récupéré avec fetch , et traduction en JSON
         .then(function (data) {   //  Etablissement d'une fonction avec les données
             data.forEach((item) => { // Pour chaque item une fonction
-                const { name, price, _id, description, imageUrl } = item; // Constante créé pour chaque item récupéré de l'api. Chaque item en contient 5 autres.
+                const { name, price, description, imageUrl, _id } = item; // Constante créé pour chaque item récupéré de l'api. Chaque item en contient 5 autres.
                 inputJs.innerHTML += // Transformation et imbrication en HTML , et ajout des 5 spécifité des items a chaque fois jusqu'as épuisement
                     ` <div class="container col-md-12 col-lg-6 col-xl-6">
                    <div class=" product mt-5 card card-border">
                       <img class="card-img-top" src="${imageUrl}"  alt="appareil photo vintage${name}">
                       <div class="card-body">
-                         <h3 class="card-title">${name}</h3>
-                         <h4>${price / 100}€</h4></p>                                                   
+                         <h2 class="card-title">${name}</h2>
+                         <h3>${price / 100}€</h3>                                                 
                          <p class="card-txt">${description}</p>
                          <div class="text-center mt-5"><a><button type="button" 
                          class="btn btn-outline-warning text-dark" 
@@ -26,7 +26,7 @@ function funRequest() { // Execution fonction de requete
         });
 
 }
-funRequest(); 
+funRequest();
 
 
 
